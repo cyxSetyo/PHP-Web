282a029205304b19f9d31f5bed35ad7c0660d566
cyxSetyo <98861415+cyxSetyo@users.noreply.github.com>
2022-10-18 16:31:16 +0700

    18 10 2022

---
+++ form.php
@@ -0,0 +1,17 @@
+<html>
+    <body>
+        <h1>Register</h1>
+        <form action="post.php" method="post">
+        <label for="first_name">First Name :
+            <input type="text" name="fisrt_name">
+        </label>
+        <br/>
+
+        <label for="last_name">Last Name :
+            <input type="text" name="last_name">
+        </label>
+        <br>
+        <input type="submit" value="Register">
+        </form>
+    </body>
+</html>
